338b926ec50d9f0d62d828ecf46c7e5f8c96fd09
wangding <408542507@qq.com>
2019-04-11 13:13:47 +0800

    chore: 使用 dlg-common.css 重构代码

--- com/dlg-goto/dlg-goto.js
+++ com/dlg-goto/dlg-goto.js
@@ -1,27 +1,26 @@
 /* exported $dlgGoto */
 var $dlgGoto = (function() {
-  var DOM = ''
-        + '<div class="notepad-dlg-goto">'
-          + '<div class="dialogbox">'
-            + '<div class="titlebar">'
+  var $dlg = $(''
+        + '<div class="notepad-dlg-mask notepad-dlg-goto">'
+          + '<div class="dialogbox notepad-dlgbox">'
+            + '<div class="notepad-dlg-titlebar">'
               + '<p class="title">转到指定行</p>'
               + '<span class="close-btn">✖</span>'
             + '</div>'
-            + '<div class="main">'
+            + '<div class="main notepad-dlg-main">'
               + '<label for="">行号(L):</label><br>'
               + '<input class="txt-line-num" type="text" autofocus><br>'
-              + '<input class="btn-goto" type="button" value="转到">'
-              + '<input class="btn-cancel" type="button" value="取消">'
+              + '<input class="btn-goto btn" type="button" value="转到">'
+              + '<input class="btn-cancel btn" type="button" value="取消">'
             + '</div>'
           + '</div>'
-        + '</div>';
-
-  var $dlg = $(DOM);
+        + '</div>');
 
   var $btnClose = $dlg.find('.close-btn'),
       $btnCancel = $dlg.find('.btn-cancel'),
       $btnGoto = $dlg.find('.btn-goto'),
-      $txtLineNum = $dlg.find('.txt-line-num');
+      $txtLineNum = $dlg.find('.txt-line-num'),
+      $titleBar = $dlg.find('.notepad-dlg-titlebar');
 
   var cfg = {
     lineNum: 1,
@@ -32,9 +31,9 @@ var $dlgGoto = (function() {
   function destoryDlg() { $dlg.remove(); }
 
   function gotoHandler() {
-    if(!validateLineNum()) return;
+    if(!validate()) return;
 
-    cfg.gotoHandler(); 
+    cfg.gotoHandler($txtLineNum.val()); 
     destoryDlg();
   }
 
@@ -46,26 +45,31 @@ var $dlgGoto = (function() {
   }
 
   function showErrMsg() {
-    alert('show Error Message!');   // TODO: 需要改成气泡框提示错误信息
+    alert('你只能在此输入数字！');   // TODO: 需要改成气泡框提示错误信息
   }
 
-  function validateLineNum() {
+  function validate() {
+    if($txtLineNum.val() === '') {
+      alert('请输入要转到的行号！');
+      return false;
+    }
+
     var n = Number($txtLineNum.val());
 
     if(n === 0 || n > cfg.totalLine) {
-      alert('行数超过了总行数');  // TODO：需要改成自己的对话框
+      alert('行数超过了总行数！');    // TODO：需要改成自己的对话框
       $txtLineNum.select();
       return false;
-    } else {
-      return true;
     }
+
+    return true;
   }
 
   function show(conf) {
     $.extend(cfg, conf);
 
     $('body').append($dlg);
-    $dlg.find('.dialogbox').draggable({handle: $dlg.find('.titlebar')});
+    $dlg.find('.dialogbox').draggable({handle: $titleBar});
 
     $btnClose.click(destoryDlg);
     $btnCancel.click(destoryDlg);
