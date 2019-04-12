78ee9d0f1b34f489e8f512c5595215007c8662b5
wangding <408542507@qq.com>
2019-04-12 13:10:59 +0800

    feat: 完成状态栏组件封装

--- com/statusbar/statusbar.js
+++ com/statusbar/statusbar.js
@@ -1,12 +1,38 @@
-/* exported $statusbar */
-var $statusbar = (function() {
-  function show() {
-    alert('hello statusbar dialog!');
+/* exported $statusBar */
+var $statusBar = (function() {
+  var $stBar = $(''
+   + '<div class="notepad-statusbar">'
+    + '<div class="left-panel"></div>'
+    + '<div class="right-panel">'
+      + '<p class="row-col"></p>'
+    + '</div>'
+   + '</div>');
+
+  var $rowCol = $stBar.find('.row-col'),
+      strRowCol = '第&nbsp;x&nbsp;行，第&nbsp;y&nbsp;列',
+      cfg = {row: 1, col: 1};
+
+  function display(isVisable) {
+    if(isVisable) {
+      $stBar.css('display', 'block');
+    } else {
+      $stBar.css('display', 'none');
+    }
   }
 
-  return {show: show};
-})();
+  function setRowCol(r, c) {
+    $rowCol.html(strRowCol.replace('x', r).replace('y', c));
+  }
 
-$(function() {
-  $statusbar.show();
-});
+  function init(conf) {
+    $.extend(cfg, conf);
+    setRowCol(cfg.row, cfg.col);
+    $('body').append($stBar);
+  }
+
+  return {
+    init: init, 
+    setRowCol: setRowCol,
+    display: display
+  };
+}());
