34ea03f5a9e4c2b3c8efc92a7b5775bbfe8fd036
wangding <408542507@qq.com>
2019-04-16 08:58:57 +0800

    feat: 菜单栏组件封装

--- com/menubar/menubar.js
+++ com/menubar/menubar.js
@@ -1,12 +1,156 @@
 /* exported $menubar*/
 var $menubar = (function() {
-  function show() {
-    alert('hello menubar dialog!');
+  var $bar = $('<div class="notepad-menubar"></div>');
+
+  var menuData,           // 所有菜单数据
+      menus = [];         // 存放五个下拉菜单的 DOM 对象
+
+  /* 下拉菜单是否展开，没有展开为：-1
+   * 展开为：n，n 代表展开的是第几个菜单
+   * 0 是文件菜单，1 是编辑菜单，2 是格式菜单
+   * 3 是查看菜单，4 是帮助菜单 */
+  var active = -1;
+
+  function createMenuTitle() {
+    var $titles = $('<ul class="menu-title"></ul>');
+
+    for(var i=0; i<menuData.length; i++) {
+      var $title = $('<li class="title"></li>');
+
+      $title.html(menuData[i].title);
+      $title.attr('data-id', i);
+      $titles.append($title);
+
+      $title.click(function() {
+        var i = Number(this.dataset.id);
+
+        if(active === -1) {
+          menus[i].css({ display: 'inline-block' });
+          active = i;
+        } else if(active !== i) {
+          menus[active].css({ display: 'none' });
+          menus[i].css({ display: 'inline-block' });
+          active = i;
+        } else {
+          menus[active].css({ display: 'none' });
+          active = -1;
+        }
+      });
+
+      $title.hover(function() {
+        if(active !== -1) {
+          var i = Number(this.dataset.id);
+
+          menus[active].css({ display: 'none' });
+          menus[i].css({ display: 'inline-block' });
+          active = i;
+        }
+      });
+    }
+
+    $bar.append($titles);
   }
 
-  return {show: show};
-})();
+  function createMenus() {
+    for(var i=0; i<menuData.length; i++) {
+      var $menus = $('<ul class="menus"></ul>'),
+          items = menuData[i].menuItems;
 
-$(function() {
-  $menubar.show();
-});
+      for(var j=0; j<items.length; j++) {
+        if(items[j].title === 'hr') {
+          var $hr = $('<li class="menu-hr"></li>');
+          $menus.append($hr);
+          continue;
+        }
+
+        var $menu = $('<li class="menu-item"></li>');
+
+        $menu.html(items[j].title);
+        $menu.attr('data-x', i);
+        $menu.attr('data-y', j);
+
+        if(items[j].shortcut !== '') {
+          var $shorcut = $('<span class="shortcut"></span>');
+
+          $shorcut.html(items[j].shortcut);
+          $menu.append($shorcut);
+        }
+
+        if(!items[j].enabled) $menu.addClass('disabled');
+
+        $menus.append($menu);
+
+        $menu.click(function() {
+          if($(this).hasClass('disabled')) return;
+
+          var i = this.dataset.x, j = this.dataset.y;
+
+          menuData[i].menuItems[j].handler();
+          menus[i].css({display: 'none'});
+          active = -1;
+        });
+      }
+
+      $menus.css({
+        width: menuData[i].width,
+        left: menuData[i].left,
+        display: 'none'
+      });
+
+      $bar.append($menus);
+      menus.push($menus);
+    }
+  }
+
+  /**
+   * 设置菜单项是否为勾选状态
+   *
+   * @param row [0-4] 代表文件、编辑、格式、查看、帮助五个菜单栏
+   * @param col 代表第几个下拉菜单项
+   * @param isEnabled true 为勾选，false 为取消勾选
+   */
+  function checked(row, col, isChecked) {
+    var menuItem = menus[row].find('.menu-item')[col];
+
+    if(isChecked) {
+      $(menuItem).prepend($('<span class="checked">✓</span>')[0]);
+    } else {
+      $(menuItem).find('.checked').remove();
+    }
+  }
+
+  /**
+   * 设置菜单项为启用或禁用状态
+   *
+   * @param row [0-4] 代表文件、编辑、格式、查看、帮助五个菜单栏
+   * @param col 代表第几个下拉菜单项
+   * @param isEnabled true 为启用，false 为禁用
+   */
+  function enabled(row, col, isEnabled) {
+    var menuItem = menus[row].find('.menu-item')[col];
+
+    if(isEnabled) {
+      $(menuItem).removeClass('disabled');
+    } else {
+      $(menuItem).addClass('disabled');
+    }
+  }
+
+  function init() {
+    createMenuTitle();
+    createMenus();
+
+    $('body').append($bar);
+  }
+
+  function show(data) {
+    menuData = data;
+    init();
+  }
+
+  return {
+    show: show,
+    checked: checked,
+    enabled: enabled
+  };
+}());
--- js/app.js
+++ js/app.js
@@ -9,13 +9,15 @@ np.config = {
   'appContainer': '.notepad-app'
 };
 
-np.bShowStatusBar = true;   // 是否显示状态栏
-np.bLineWrap = false;       // 是否换行
+np.bShowStatusBar = false;   // 是否显示状态栏
+np.bLineWrap = false;        // 是否换行
 
 /* global $menubar $editor $statusbar: true */
+/*
 $(function() {
   $menubar.show();
   $editor.show();
   //$statusbar.init({'row': 1, 'col': 1});
   $statusbar.show();
 });
+*/
